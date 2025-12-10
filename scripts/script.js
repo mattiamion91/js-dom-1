fd2e7be0f398888ad6774e4c8c8b35f1547439a4
Mattia Mion <mattia.mion@gmail.com>
2025-12-10 17:49:57 +0100

    feat: esercizo fatto anche con bonus

--- scripts/script.js
+++ scripts/script.js
@@ -2,33 +2,31 @@ const buttonToOn = document.getElementById('accensione') //seleziono elemento do
 console.log(buttonToOn); //check in console per capire se ho selezionato l'elemento giusto
 const lampadina = document.getElementById('foto-lampadina') //seleziono elemento dom usando id
 
-buttonToOn.addEventListener('click', function(){ //aggancio una funzione che quando premo pulsante mi cambia sia src immagine sia alt immagine
-    
-    lampadina.src = './img/yellow_lamp.png'
-    
-    lampadina.alt = 'lampadina-accesa'
+let lampadinaAccesa = false
 
-    buttonToOn.innerHTML = '<button id="accensione">SPEGNI</button>'
+buttonToOn.addEventListener('click', function () { //aggancio una funzione che quando premo pulsante mi cambia sia src immagine sia alt immagine
+    if (lampadinaAccesa === false) { //se la lampadina é spenta...
+        
+        lampadina.src = './img/yellow_lamp.png' //cambio l'immagine cambiando src
 
+        lampadina.alt = 'lampadina-accesa' //cambio l'alt
+
+        buttonToOn.innerHTML = '<button id="accensione">SPEGNI</button>' //cambio cio che c'e scritto nel bottone
+
+        lampadinaAccesa = true // cambio la variabile booleana in true perche la lampadina si é accecsa
     
+    } else { //se la lampadina é accesa
+        
+        lampadina.src = './img/white_lamp.png' //ritorno all'immagine di lampadina spenta
+
+        lampadina.alt = 'lampadina-spenta' //ritorno alt lampadina spenta
+
+        buttonToOn.innerHTML = '<button id="accensione">ACCENDI</button>' //ritorno alla scritta accendi perche ormai la lampadina é spenta
+
+        lampadinaAccesa = false //ormai la lampadina é spenta qundi la mia variabile booleana é falsa
+
+    }
+
     console.log('cliccato'); //check in console
-    
+
 })
-
-
-//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-/*
-const buttonToOff = document.getElementById('spegnimento') //seleziono elemento dom usando id
-console.log(buttonToOff); //check in console per capire se ho selezionato l'elemento giusto
-const lamp = document.getElementById('lamp-pic') //seleziono elemento dom usando id
-
-buttonToOff.addEventListener('click', function(){ //aggancio una funzione che quando premo pulsante mi cambia sia src immagine sia alt immagine
-    
-    lamp.src = './img/white_lamp.png'
-    
-    lamp.alt = 'off-lamp' 
-    
-    console.log('cliccato'); //check in console
-    
-})
-*/
